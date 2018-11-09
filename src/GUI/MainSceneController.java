380bceef3d0274886bf3909c9fde97240d014c80
Alex Coupe <acoupe@gmail.com>
2018-11-09 13:43:51 +0000

    Quit button improved; script implemented

--- src/GUI/MainSceneController.java
+++ src/GUI/MainSceneController.java
@@ -5,27 +5,37 @@
  */
 package GUI;
 
+import Commands.CommandHandler;
+import java.awt.Label;
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.FileChooser;
 
 /**
  *
  * @author paul
  */
+
+
 public class MainSceneController implements Initializable {
     
+    CommandHandler comHandler = new CommandHandler();
+    
     @FXML
-    BorderPane rootPane;
+    BorderPane borderPane;
        
     @FXML
     private void handleOpsButton(ActionEvent event) throws IOException {
@@ -35,12 +45,20 @@ public class MainSceneController implements Initializable {
     
     @FXML
     private void handleNewButton(ActionEvent event) throws IOException {
-        System.out.println("You clicked me!");
+        AnchorPane pane = FXMLLoader.load(getClass().getResource("NewMatrixScene.fxml"));
     }
     
     @FXML
     private void handleLoadButton(ActionEvent event) {
-        System.out.println("You clicked me!");
+        final Label fileLabel = new Label();
+        FileChooser load = new FileChooser();
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("CSV files (*.csv)", "*.csv");
+        load.getExtensionFilters().add(extFilter);
+        File file = load.showOpenDialog(null);   
+        if (file != null)
+        {
+            fileLabel.setText(file.getPath());
+        }
     }
     
     @FXML
@@ -49,17 +67,49 @@ public class MainSceneController implements Initializable {
     }
     @FXML
     private void handleScriptButton(ActionEvent event) {
-        System.out.println("You clicked me!");
+       
+        final Label fileLabel = new Label();
+        FileChooser load = new FileChooser();
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("TXT files (*.txt)", "*.txt");
+        load.getExtensionFilters().add(extFilter);
+        File file = load.showOpenDialog(null);   
+        if (file != null)
+        {
+            fileLabel.setText(file.getPath());
+        }
+        
+        String[] commAndArg = new String[2] ;
+        commAndArg[0] = "script";
+        commAndArg[1] = fileLabel.getText();
+        comHandler.executeCommands(commAndArg);
+        
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setTitle("MuttLab");
+        alert.setHeaderText("Load Script");
+        alert.setContentText("Script has been run");
+
+        alert.showAndWait();
     }
     
     @FXML
     private void handleQuitButton(ActionEvent event) {
-       Platform.exit();
+        
+        Alert alert = new Alert(AlertType.CONFIRMATION);
+        alert.setTitle("MuttLab");
+        alert.setHeaderText("Quit");
+        alert.setContentText("Are you want to quit?");
+
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.get() == ButtonType.OK)
+        {
+            Platform.exit();
+        }
+        else
+            event.consume();
     }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+    }
         
 }
