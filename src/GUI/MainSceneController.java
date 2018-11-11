698e4fdd50569d0e21657f53b3e0f42c96817a1b
Alex Coupe <acoupe@gmail.com>
2018-11-11 12:50:45 +0000

    New Matrix scene fully styled. Back and clear buttons also work

--- src/GUI/MainSceneController.java
+++ src/GUI/MainSceneController.java
@@ -22,7 +22,6 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
-import javafx.scene.layout.AnchorPane;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
--- src/GUI/NewMatrixSceneController.java
+++ src/GUI/NewMatrixSceneController.java
@@ -5,9 +5,18 @@
  */
 package GUI;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -19,6 +28,42 @@ public class NewMatrixSceneController implements Initializable {
     /**
      * Initializes the controller class.
      */
+    @FXML
+    private Button back;
+    
+    @FXML
+    private TextField display;
+    
+    @FXML
+    private void input(ActionEvent event) throws IOException {
+       display.setText("Bla");
+    }
+    
+    @FXML
+    private void clear(ActionEvent event) throws IOException {
+        display.clear();
+    }
+    
+    @FXML
+    private void newMat(ActionEvent event) throws IOException {
+        //call command handler add on the string matrix
+        //may need to do some checking here for valid format
+    }
+    
+    @FXML
+    private void back(ActionEvent event) throws IOException {
+    
+    Stage stage; 
+    Parent root;
+    stage=(Stage)back.getScene().getWindow();
+     
+    root = FXMLLoader.load(getClass().getResource("MainScene.fxml"));
+       
+    Scene scene = new Scene(root);
+    stage.setScene(scene);
+    stage.show();
+    }
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
