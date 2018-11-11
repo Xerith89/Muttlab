d4a7caa9406f16f3770eb7c68256d7286dbd51b9
Alex Coupe <acoupe@gmail.com>
2018-11-11 11:18:54 +0000

    All buttons now working on main menu - New Matrix and Operations now switch to the correct respective scenes.

--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -20,6 +20,7 @@ public class Main extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
+        
         Parent root = FXMLLoader.load(getClass().getResource("MainScene.fxml"));
         
         Scene scene = new Scene(root);
--- src/GUI/MainSceneController.java
+++ src/GUI/MainSceneController.java
@@ -6,7 +6,6 @@
 package GUI;
 
 import Commands.CommandHandler;
-import java.awt.Label;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
@@ -17,11 +16,15 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
 /**
  *
@@ -32,16 +35,40 @@ import javafx.stage.FileChooser;
 public class MainSceneController implements Initializable {
     
     CommandHandler comHandler = new CommandHandler();
+    
+    @FXML
+    private Button selectOps;
            
     @FXML
+    private Button selectNew;
+    
+    @FXML
     private void handleOpsButton(ActionEvent event) throws IOException {
-        AnchorPane pane = FXMLLoader.load(getClass().getResource("OperationsScene.fxml"));
-             
+    
+    Stage stage; 
+    Parent root;
+    stage=(Stage)selectOps.getScene().getWindow();
+     
+    root = FXMLLoader.load(getClass().getResource("OperationsScene.fxml"));
+       
+    Scene scene = new Scene(root);
+    stage.setScene(scene);
+    stage.show();
+    
     }
     
     @FXML
     private void handleNewButton(ActionEvent event) throws IOException {
-        AnchorPane pane = FXMLLoader.load(getClass().getResource("NewMatrixScene.fxml"));
+    
+    Stage stage; 
+    Parent root;
+    stage=(Stage)selectNew.getScene().getWindow();
+     
+    root = FXMLLoader.load(getClass().getResource("NewMatrixScene.fxml"));
+       
+    Scene scene = new Scene(root);
+    stage.setScene(scene);
+    stage.show();
     }
         
     @FXML
